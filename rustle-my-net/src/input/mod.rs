ae7f389b1c7ed02e885de336070b8397156c60e6
Tom Schneider <toschneider@uos.de>
2017-01-26 23:41:22 +0100

    input works

--- rustle-my-net/src/input/mod.rs
+++ rustle-my-net/src/input/mod.rs
@@ -1,3 +1,29 @@
-fn read() -> u8 {
-
+use models::Flower;
+use std::io::{BufReader, BufRead, Result};
+use std::fs::File;
+use std::path::Path;
+pub fn read(filename: &Path) -> Result<Vec<Flower>> {
+    println!("{:?}", filename.display());
+    let f = try!(File::open(filename));
+    let mut reader = BufReader::new(&f);
+    let mut flowers = Vec::new();
+    let mut line = String::new();
+    let mut len = 1;
+    while len > 0 {
+        len = match reader.read_line(&mut line) {
+            Ok(val) => val,
+            Err(_) => return Ok(flowers),
+        };
+        let split = line.split(',');
+        let content = split.collect::<Vec<&str>>();
+        let mut s = content[4].to_string();
+        s.pop();
+        s.pop();
+        flowers.push(Flower::new(s,
+                                 content[0].parse::<f32>().unwrap(),
+                                 content[1].parse::<f32>().unwrap(),
+                                 content[2].parse::<f32>().unwrap(),
+                                 content[3].parse::<f32>().unwrap()));
+    }
+    Ok(flowers)
 }
--- rustle-my-net/src/main.rs
+++ rustle-my-net/src/main.rs
@@ -1,4 +1,13 @@
 mod input;
+mod models;
+use input::read;
+use models::Flower;
+use std::path::Path;
 fn main() {
-    println!("Hello, world!");
+    let filename = Path::new("/home/tom/pir-project2017/rustle-my-net/data/iris_flowers.txt");
+    let flowers = match read(&filename) {
+        Ok(val)  => val,
+        Err(e) => panic!("reading file {} failed. Error {:?}",filename.display(), e),
+    };
+    println!("flower: {}",flowers[150]);
 }
