c35d9a73221f0d6a2ca9e7a7636db0ce91897b4c
Tom Schneider <toschneider@uos.de>
2017-01-27 14:06:01 +0100

    wip clap should work

--- rustle-my-net/src/main.rs
+++ rustle-my-net/src/main.rs
@@ -1,8 +1,31 @@
 mod input;
 mod structs;
+extern crate clap;
+use clap::{Arg, App, SubCommand};
 use input::read;
 use std::env;
 fn main() {
+    let matches = App::new("rustle my net")
+        .subcommand(SubCommand::with_name("learn"))
+        .subcommand(SubCommand::with_name("classify"))
+        .arg(Arg::with_name("data")
+             .long("data")
+             .short("d")
+             .takes_value(true)
+             .default_value("data/iris_flowers.txt"))
+        .arg(Arg::with_name("config")
+             .long("config")
+             .short("c")
+             .takes_value(true)
+             .default_value("data/iris_flowers.txt"))
+        .get_matches();
+
+    let data: String = matches.value_of("data").unwrap().parse().unwrap();
+    let config: String = matches.value_of("config").unwrap().parse().unwrap();
+    if matches.subcommand_name().is_none() {
+        println!("Provide at least one subcommand: learn or classify");
+        return;
+    }
     let path = env::current_dir().unwrap();
-    let filename = path.join("data/iris_flowers.txt");
+    let filename = path.join(data);
 }
