b1ccd3aee9b74b9f45f447a0a0945558de5d385e
Tom Schneider <toschneider@uos.de>
2017-01-30 15:32:36 +0100

    parsing of commandline arguments is now in input

--- rustle-my-net/src/input/mod.rs
+++ rustle-my-net/src/input/mod.rs
@@ -2,6 +2,11 @@ use structs::{Flower, FlowerName};
 use std::io::{BufReader, BufRead, Result};
 use std::fs::File;
 use std::path::Path;
+extern crate clap;
+use self::clap::{Arg, App, SubCommand};
+
+// reads content of given file and returns a result with
+// either the Vector of Flowers or Err
 pub fn read(filename: &Path) -> Result<Vec<Flower>> {
     let f = try!(File::open(filename));
     let reader = BufReader::new(&f);
@@ -9,18 +14,59 @@ pub fn read(filename: &Path) -> Result<Vec<Flower>> {
     for line in reader.lines() {
         let l = line.unwrap();
         let content = l.split(',').collect::<Vec<&str>>();
-        flowers.push(Flower::new(
-            match content[4] {
-                "Iris-setosa"  => FlowerName::IrisSetosa,
-                "Iris-versicolor" => FlowerName::IrisVersicolor,
-                "Iris-virginica" => FlowerName::IrisVirginica,
-                _ => panic!("unknown flowertype!!!")
-            },
-            content[0].parse::<f32>().unwrap(),
-            content[1].parse::<f32>().unwrap(),
-            content[2].parse::<f32>().unwrap(),
-            content[3].parse::<f32>().unwrap(),
-        ));
+        // Expected Flower?
+        match (match content[4] {
+            "Iris-setosa"  => Ok(FlowerName::IrisSetosa),
+            "Iris-versicolor" => Ok(FlowerName::IrisVersicolor),
+            "Iris-virginica" => Ok(FlowerName::IrisVirginica),
+            _ => Err("Invalid flower"),
+        },
+               content[0].parse::<f32>(),
+               content[1].parse::<f32>(),
+               content[2].parse::<f32>(),
+               content[3].parse::<f32>()) {
+            // push if valid Flower
+            (Ok(name),
+             Ok(sepal_length),
+             Ok(sepal_width),
+             Ok(petal_length),
+             Ok(petal_width)) => flowers.push(Flower::new(
+                 name,
+                 sepal_length,
+                 sepal_width,
+                 petal_length,
+                 petal_width,
+             )),
+            // print Err if invalid line
+            _ =>  println!("this is not a valid flower {:?}", content),
+        };
+
+
+
+
     }
     Ok(flowers)
+}
+pub fn commands() -> (String,String, String) {
+    let matches = App::new("rustle my net")
+        .subcommand(SubCommand::with_name("learn"))
+        .subcommand(SubCommand::with_name("classify"))
+        .arg(Arg::with_name("data")
+             .long("data")
+             .short("d")
+             .takes_value(true)
+             .default_value("data/iris_flowers.txt"))
+        .arg(Arg::with_name("config")
+             .long("config")
+             .short("c")
+             .takes_value(true)
+             .default_value("data/iris_flowers.txt"))
+        .get_matches();
+    if matches.subcommand_name().is_none() {
+        panic!("Provide at least one subcommand: learn or classify");
+    }
+    (matches.value_of("data").unwrap().parse().unwrap(),
+      matches.value_of("config").unwrap().parse().unwrap(),
+      matches.subcommand_name().unwrap().to_string()
+    )
 }
--- rustle-my-net/src/main.rs
+++ rustle-my-net/src/main.rs
@@ -1,31 +1,12 @@
 mod input;
 mod structs;
-extern crate clap;
-use clap::{Arg, App, SubCommand};
-use input::read;
+
+use input::{read, commands};
 use std::env;
 fn main() {
-    let matches = App::new("rustle my net")
-        .subcommand(SubCommand::with_name("learn"))
-        .subcommand(SubCommand::with_name("classify"))
-        .arg(Arg::with_name("data")
-             .long("data")
-             .short("d")
-             .takes_value(true)
-             .default_value("data/iris_flowers.txt"))
-        .arg(Arg::with_name("config")
-             .long("config")
-             .short("c")
-             .takes_value(true)
-             .default_value("data/iris_flowers.txt"))
-        .get_matches();
 
-    let data: String = matches.value_of("data").unwrap().parse().unwrap();
-    //let config: String = matches.value_of("config").unwrap().parse().unwrap();
-    if matches.subcommand_name().is_none() {
-        println!("Provide at least one subcommand: learn or classify");
-        return;
-    }
+    let (data, config, subcom) = commands();
+
     let path = env::current_dir().unwrap();
     let filename = path.join(data);
     let input = match read(&filename) {
@@ -35,4 +16,5 @@ fn main() {
     for i in input {
         println!("{}",i);
     }
+    println!("{:?}", config);
 }
