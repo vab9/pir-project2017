1260d1095e0f5fec0fedb476c41ac7f9fbaf9ca1
johann162 <joarndt@uos.de>
2017-02-01 17:01:20 +0100

    improved readability and added comments

--- rustle-my-net/src/structs/mod.rs
+++ rustle-my-net/src/structs/mod.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 
+
+/// enum flower names
 #[derive(Debug)]
 pub enum FlowerName {
     IrisSetosa,
@@ -7,6 +9,7 @@ pub enum FlowerName {
     IrisVirginica,
 }
 
+/// flowertype that contains the 4 inputs and the Flowername
 #[derive(Debug)]
 pub struct Flower {
     name: FlowerName,
@@ -16,19 +19,28 @@ pub struct Flower {
     petal_width: f32,
 }
 
+/// constuctor for the Flowertype
 impl Flower {
     pub fn new(n: FlowerName, sl: f32, sw: f32, pl: f32, pw: f32) -> Flower {
-        Flower { name: n,
-                 sepal_length: sl,
-                 sepal_width: sw,
-                 petal_length: pl,
-                 petal_width: pw,
+        Flower {
+            name: n,
+            sepal_length: sl,
+            sepal_width: sw,
+            petal_length: pl,
+            petal_width: pw,
         }
     }
 }
+
+/// easy printing of the flower type
 impl fmt::Display for Flower {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({:?}, {}, {}, {}, {})", self.name, self.sepal_length,
-               self.sepal_width, self.petal_length, self.petal_width)
+        write!(f,
+               "({:?}, {}, {}, {}, {})",
+               self.name,
+               self.sepal_length,
+               self.sepal_width,
+               self.petal_length,
+               self.petal_width)
     }
 }
