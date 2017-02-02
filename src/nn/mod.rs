e6db13ce49a6395c91a9a42a4508e1ae90fad492
johann162 <joarndt@uos.de>
2017-02-02 17:10:21 +0100

    added some comments and changed nn types for nalgebra

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -1,17 +1,22 @@
 use rand::distributions::normal::Normal;
 use rand;
 use rand::distributions::IndependentSample;
-use nalgebra::DMatrix;
+use nalgebra::{DMatrix, DVector};
 
 
 /// Network type which shows how the Network looks like
 pub struct Network {
+    /// a Vec giving the sizes of the layers
+    /// the first entry corresponds to the inputlayer
+    /// and the last entry corresponds to the outputlayer
     layers: Vec<u8>,
+    /// a Vec that contains the weights of the respective layer
     weights: Vec<DMatrix<f32>>,
-    biases: Vec<Vec<f32>>,
+    ///
+    biases: Vec<DVector<f32>>,
 }
 
-/// Implements Constructor and some getter functions
+
 impl Network {
     /// build a new Network by given parameters
     pub fn new(sizes: Vec<u8>) -> Result<Network, &'static str> {
@@ -24,15 +29,19 @@ impl Network {
         // Store the weights and biases in lists
         // We will not need weights or biases for input layer, so ignore that (hence -1)
         let mut weights: Vec<DMatrix<f32>> = Vec::with_capacity(sizes.len() - 1);
-        let mut biases: Vec<Vec<f32>> = Vec::with_capacity(sizes.len() - 1);
+        let mut biases: Vec<DVector<f32>> = Vec::with_capacity(sizes.len() - 1);
 
         let mut rng = rand::thread_rng();
         let normal = Normal::new(0.0, 1.0);
 
         for i in 1..sizes.len() {
-            biases.push(Vec::with_capacity(sizes[i] as usize));
-            for _ in 0..sizes[i] {
-                biases[i - 1].push(normal.ind_sample(&mut rng) as f32);
+
+            unsafe {
+                biases.push(DVector::new_uninitialized(sizes[i] as usize));
+            }
+
+            for j in 0..sizes[i] {
+                biases[i - 1][j as usize] = normal.ind_sample(&mut rng) as f32;
             }
             weights.push(DMatrix::new_random_normal(sizes[i - 1] as usize, sizes[i] as usize));
         }
@@ -45,15 +54,18 @@ impl Network {
         })
     }
 
+    /// returns the layers
     pub fn get_layers(&self) -> &Vec<u8> {
         &self.layers
     }
 
+    /// returns the weights
     pub fn get_weights(&self) -> &Vec<DMatrix<f32>> {
         &self.weights
     }
 
-    pub fn get_biases(&self) -> &Vec<Vec<f32>> {
+    /// returns the biases
+    pub fn get_biases(&self) -> &Vec<DVector<f32>> {
         &self.biases
     }
 }
@@ -63,8 +75,9 @@ trait Random {
     fn new_random_normal(rows: usize, columns: usize) -> DMatrix<f32>;
 }
 
-/// Implements random number initializer for DMatrix
+
 impl Random for DMatrix<f32> {
+    /// Implements random number initializer for DMatrix
     fn new_random_normal(rows: usize, columns: usize) -> DMatrix<f32> {
 
         // Init the DMatrix with rows and columns
