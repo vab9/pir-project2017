235cb5363bc8c327ce0224842ce5b0033e45404e
Lukas Kalde <lkalde@uos.de>
2017-02-09 12:41:07 +0100

    Implemented some structues around backpropagation, still heavily  WIP

---
+++ src/nn/learning.rs
@@ -0,0 +1,42 @@
+pub fn sgd(net: nn::Network, training_data: Vec<Data>, epochs: u8, mini_batch_size: u8, eta: f32) {
+    use rand;
+    let mut rng = rand::thread_rng();
+
+    for j in 0..epochs {
+        rng.shuffle(training_data);
+        //let mini_batches = training_data.chunks(mini_batch_size);
+        for mini_batch in training_data.chunks(mini_batch_size) {
+            nn.update_mini_batch(mini_batch, eta);
+        }
+        println!("Epoch {} complete!", j);
+    }
+
+}
+
+
+
+/*
+def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+        """Train the neural network using mini-batch stochastic
+        gradient descent.  The "training_data" is a list of tuples
+        "(x, y)" representing the training inputs and the desired
+        outputs.  The other non-optional parameters are
+        self-explanatory.  If "test_data" is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially."""
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test)
+            else:
+                print "Epoch {0} complete".format(j)*/
